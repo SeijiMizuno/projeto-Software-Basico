8479052c682968f23948c64d90bfebd81b9a417e
SeijiMizuno <senji.mizuno@gmail.com>
2025-06-08 23:54:07 -0300

    WIP: criado procedimento que procura espaço livre para alocação. (FALTA FAZER COMENTÁRIOS "TO DO")

--- alocar.s
+++ alocar.s
@@ -1,19 +1,22 @@
 .section .data
-    inicioHeap: .quad 0
-    topoHeap: .quad 0
+    INICIO_HEAP: .quad 0
+    TOPO_HEAP: .quad 0
 
     # strings de impressão
-    msgHeap:            .ascii "Imagem da heap: "
-    .equ msgHeap_len, . - msgHeap           # equivalente ao comprimento da string
+    MSG_IMAGEM_HEAP:            .ascii "Imagem da heap: "
+    .equ MSG_IMAGEM_HEAP_LEN, . - MSG_IMAGEM_HEAP           # Comprimento da string
 
-    charOcupado:        .ascii "+"
-    charLivre:          .ascii "-"
-    charNodeManager:    .ascii "#"
+    CHAR_OCUPADO:       .ascii "+"
+    CHAR_LIVRE:         .ascii "-"
+    CHAR_NODEMANAGER:   .ascii "#"
 
-    newline:            .byte 10        # em ASCII, byte 10 = '/n'
+    CHAR_NEWLINE:       .byte 10        # em ASCII, byte 10 = '/n'
 
-    a:          .quad 0                 # ponteiro para bloco alocado de 10 bytes
-    b:          .quad 0                 # ponteiro para bloco alocado de 4 bytes
+    A:          .quad 0
+    B:          .quad 0
+    C:          .quad 0
+
+    TAMANHO:    .quad 0                 # variável global que armazena o tamanho do bloco a ser alocado
 
 .section .text
 
@@ -23,130 +26,199 @@
 .globl  iniciaAlocador
 .type   iniciaAlocador,@function
 iniciaAlocador:
-    mov     $0, %rdi            # coloca 0 em rdi
-    mov     $12, %rax           # numero do syscall = 12 em rax
+    movq     $0, %rdi            # coloca 0 em rdi
+    movq     $12, %rax           # numero do syscall = 12 em rax
     syscall                     # salta para o kernel, que retorna em rax o endereço atual do "break" do heap
-    mov     %rax, inicioHeap(%rip)  # inicio da lista
-    mov     %rax, topoHeap(%rip)    # fim atual do heap
+    movq     %rax, INICIO_HEAP(%rip)  # inicio da lista
+    movq     %rax, TOPO_HEAP(%rip)    # fim atual do heap
     ret                         # preserva rax e limpa o frame
 
 # ==========================================================================
 # Procedimento imprimeMapa =================================================
 # Imprime a região da heap (e.g.: 4 bytes ocupados -> ################++++)
 imprimeMapa:
-    movq $0, %rdi   # coloca 0 em rdi para retornar o valor de brk (se não for zero, brk recebe o valor de rdi)
-    movq $12, %rax  # 12 em rax = syscall altera valor rbk (rdi é 0, então ao invés de alterar, syscall apenas retorna rbk)
-    syscall         # endereço de rbk é retornado em %rax
-    movq %rax, topoHeap
-    movq inicioHeap, %rbx   # move o endereço de inicioHeap para %rbx
-    
     # === escreve "Imagem da heap: " ===
     movq $1, %rax           # syscall write
     movq $1, %rdi           # stdout
-    movq $msgHeap, %rsi
-    movq $msgHeap_len, %rdx
+    movq $MSG_IMAGEM_HEAP, %rsi
+    movq $MSG_IMAGEM_HEAP_LEN, %rdx
     syscall
-    
-whileImprime:
-    cmpq topoHeap, %rbx     # Compara endereço rbx (inicioHeap) com topoHeap
-    jge endWhileImprime     # Se rbx for maior que topoHeap, então dá um jump para endWhileImprime (chegou ao fim da Heap)
+
+    movq INICIO_HEAP(%rip), %rbx   # move o endereço de INICIO_HEAP para %rbx
+.whileImprime:
+    cmpq TOPO_HEAP(%rip), %rbx     # Compara endereço rbx (INICIO_HEAP) com rax TOPO_HEAP
+    jge .endWhileImprime     # Se rbx >= TOPO_HEAP, então dá um jump para .endWhileImprime (chegou ao fim da Heap)
 
     movq $16, %r8          # armazena 16 no contador %r8
-imprimeNodeManager:         # impressão dos caracteres '#'
+.imprimeNodeManager:         # impressão dos caracteres '#'
     cmpq $0, %r8
-    jl fimImprimeNodeManager
+    je .fimImprimeNodeManager
 
+    # printa um "#"
     movq $1, %rax           # syscall write
     movq $1, %rdi           # stdout
-    movq $charNodeManager, %rsi
+    movq $CHAR_NODEMANAGER, %rsi
     movq $1, %rdx
     syscall
-    subq $1, %r8            # decrementa rcx
-    jmp imprimeNodeManager   # repete o loop
 
-fimImprimeNodeManager:
+    subq $1, %r8            # decrementa %r8
+    jmp .imprimeNodeManager   # repete o loop
 
-    # checando se o nó está ocupado ou livre
-    movq (%rbx), %rax
-
+.fimImprimeNodeManager:
     # armazena o tamanho do nó em %r8
     movq 8(%rbx), %r8
 
     # decisão se irá imprimir '+' ou '-'
-    cmpq $1, %rax
-    je setOcupado
+    cmpq $1, (%rbx)
+    je .setOcupado
     # se chegou aqui, então o nó está livre (%rax = 0)
-    movq $charLivre, %rsi
-    jmp whilePrintBloco
-setOcupado:
-    movq $charOcupado, %rsi
+    movq $CHAR_LIVRE, %rsi
+    jmp .whilePrintBloco
+.setOcupado:
+    movq $CHAR_OCUPADO, %rsi
 
-whilePrintBloco:
+.whilePrintBloco:
     cmpq $0, %r8
-    jle nextBloco            # se r8 == 0, pula impressão
+    je .nextWhilePrintBloco            # se r8 == 0, pula impressão
 
     movq $1, %rax
     movq $1, %rdi
     movq $1, %rdx
     syscall
     subq $1, %r8
-    jmp whilePrintBloco
+    jmp .whilePrintBloco
 
-nextBloco:
-    # Avança ponteiro: 16 (controle) + tamanho do bloco
+.nextWhilePrintBloco:
+    # Avança ponteiro: 16 (node_manager) + tamanho do bloco
     movq 8(%rbx), %r8
     addq $16, %r8
     addq %r8, %rbx
+    jmp .whileImprime
 
-    jmp whileImprime
-
-endWhileImprime:
+.endWhileImprime:
     movq $1, %rax           # syscall write
     movq $1, %rdi           # stdout
-    movq $newline, %rsi
+    movq $CHAR_NEWLINE, %rsi
     movq $1, %rdx
     syscall
     ret
 
 # ==========================================================================
-# alocaMem de teste ============================================================
-alocaMem:
-    movq topoHeap(%rip), %rax     # rax = topo atual do heap
+# procuraLivre =============================================================
+# Params: 16(%rbp) = tamanho do bloco a ser alocado ========================
+procuraLivre:
+    pushq %rbp
+    movq %rsp, %rbp
 
-    movq %rdi, %rcx               # rcx = tamanho pedido (sem controle)
-    movq %rcx, %r9                # salva o tamanho para gravar depois
+    movq INICIO_HEAP(%rip), %rbx
+    movq TOPO_HEAP(%rip), %r10
 
-    addq $16, %rcx                # rcx = tamanho total com controle
-    addq %rcx, %rax               # rax = novo topo desejado
+.whileProcura:
+    cmpq %r10, %rbx
+    jge .expandeHeap
 
-    movq %rax, %rdi               # rdi = novo endereço para o brk
+    cmpq $1, (%rbx)           # checa se o nó está ocupado
+    je .nextWhileProcura    # se sim, pula para o próximo nó
+
+    # se chegou aqui é porque o nó é livre
+    movq 8(%rbx), %rcx
+    cmpq 16(%rbp), %rcx     # compara o tamanho desejado com o tamanho do nó
+    jl .nextWhileProcura           # se for menor que o tamanho solicitado, pula para o próximo nó   
+
+    # se chegou aqui é porque nó_size >= tamanho solicitado
+    # colocando o endereço do nó livre encontrado em %rax
+
+    addq $16, %rbx  # pula para o endereço do conteúdo do nó
+    movq %rbx, %rax
+    jmp .fimProcuraLivre    
+
+.nextWhileProcura:
+    # Avança ponteiro: 16 (node_manager) + tamanho do bloco
+    movq 8(%rbx), %r13
+    addq $16, %r13
+    addq %r13, %rbx
+    jmp .whileProcura
+
+# "joga" %rbk mais para frente
+.expandeHeap:
+    movq 16(%rbp), %r9               # 16(%rbp) = tamanho pedido (sem node_manager)
+
+    addq $16, %r9                # tamanho total com node_manager
+    addq %r10, %r9             # novo topo desejado
+
+    movq %r9, %rdi               # rdi = novo endereço para o brk
     movq $12, %rax                # syscall brk
     syscall
 
-    # ponteiro inicial do bloco alocado
-    movq topoHeap(%rip), %rbx
+    # Verifica se brk falhou
+    cmpq %rdi, %rax
+    jne erroMemoria
+
+    # Atualiza TOPO_HEAP
+    movq %rax, TOPO_HEAP(%rip)
+
+    # coloca o endereço do novo bloco em %rax para retorno da procedure
+    addq $16, %r10          # %r10 ainda contém o valor antigo do topo da heap
+    movq %r10, %rax     
+    jmp .fimProcuraLivre
+
+.fimProcuraLivre:
+    # fim do procedimento
+    popq %rbp
+    ret
+
+
+# ==========================================================================
+# alocaMem ============================================================
+# Params: 16(%rbp) = tamanho do nó a ser alocado ========================
+alocaMem:
+    pushq %rbp
+    movq %rsp, %rbp
+
+# TO DO =======================
+# Proteger de tentativa de alocação de tamanho ZERO, abortar ou só devolver a pilha como está
+# TO DO =======================
+
+    movq 16(%rbp), %rax
+    pushq %rax               # empilha param. tamanho_solicitado
+    call procuraLivre
+    addq $8, %rsp               # desempilha param. tamanho_solicitado
+
+# TO DO =======================
+# Processo agora é:
+    # 1. checar se o tamanho solicitado vai deixar uma "sobra" de espaço livre
+        # 1.1 se sim, checar se o espaço livre possui no mínimo 17 bytes [8][8][1]
+            # 1.1.1 se sim, separa o nó em dois, a primeira parte será ocupada pelo tamanho solicitado
+            # 1.1.2 se não, não faz nada e só ocupa todo o nó mesmo (não terá "sobra" suficiente para alocar um novo nó)
+        # 1.2 se não, o tamanho é perfeito (tamanho solicitado == tamanho livre), alocação normal.  
+# TO DO =======================
+
+    # ponteiro inicial do nó alocado
+    movq %rax, %rbx
 
     # marca como OCUPADO
-    movq $1, (%rbx)
+    movq $1, -16(%rbx)
 
-    # salva o TAMANHO do bloco (sem os 16 bytes de controle)
-    movq %r9, 8(%rbx)
+    # salva o TAMANHO do nó (sem os 16 bytes de node_manager)
+    movq 16(%rbp), %r12
+    movq %r12, -8(%rbx)
 
-    # atualiza topoHeap
-    movq %rax, topoHeap(%rip)
-
-    # retorna ponteiro de dados (16 bytes depois do controle)
-    addq $16, %rbx
+    # retorna ponteiro de dados (16 bytes depois do node_manager)
+    # addq $16, %rbx
     movq %rbx, %rax
+    jmp .fimAlocaMem
 
+# fim do procedimento
+.fimAlocaMem:
+    popq %rbp
     ret
 
 # ==========================================================================
 # desalocaMem =============================================================
-# Recebe um ponteiro de dados e marca o bloco como livre
+# Recebe um ponteiro de dados e marca o nó como livre
 # Entrada: %rdi = ponteiro de dados (retornado por alocaMem)
 desalocaMem:
-    subq $16, %rdi         # volta 16 bytes para acessar o início do bloco (controle)
+    subq $16, %rdi         # volta 16 bytes para acessar o início do nó (node_manager)
     movq $0, (%rdi)        # marca como livre (0)
     ret
 
@@ -157,23 +229,103 @@ desalocaMem:
 main:
     call iniciaAlocador
     call imprimeMapa              # imprime estado inicial da heap (vazia)
+    # Saída esperada:
+    # Imagem da heap: 
 
-    movq $2, %rdi
+    # Aloca 8 e armazena em 'A'
+    movq $8, TAMANHO
+    pushq TAMANHO
     call alocaMem
-    movq %rax, a
-    call imprimeMapa              # após alocar a
+    addq $8, %rsp
 
-    movq $3, %rdi
+    movq %rax, A
+    call imprimeMapa              # após alocar A
+    # Saída esperada:
+    # Imagem da heap: ################++++++++
+
+    # Aloca 3 e armazena em 'B'
+    movq $3, TAMANHO
+    pushq TAMANHO
     call alocaMem
-    movq %rax, b
-    call imprimeMapa              # após alocar b
+    addq $8, %rsp
 
-    # desaloca o ponteiro 'a'
-    movq a, %rdi
+    movq %rax, B
+    call imprimeMapa              # após alocar B
+    # Saída esperada:
+    # Imagem da heap: ################++++++++################+++
+
+    # desaloca o ponteiro 'A'
+    movq A, %rdi
     call desalocaMem
-    call imprimeMapa              # após desalocar a
+    call imprimeMapa              # após desalocar A
+    # Saída esperada:
+    # Imagem da heap: ################--------################+++
+
+    # Aloca 10 e armazena em 'C'
+    movq $10, TAMANHO
+    pushq TAMANHO
+    call alocaMem
+    addq $8, %rsp
+
+    movq %rax, C
+    call imprimeMapa              # após alocar C
+    # Saída esperada:
+    # Imagem da heap: ################--------################+++################++++++++++
+
+    # desaloca o ponteiro 'B'
+    movq B, %rdi
+    call desalocaMem
+    call imprimeMapa              # após desalocar B
+    # Saída esperada:
+    # Imagem da heap: ################--------################---################++++++++++
+
+    # desaloca o ponteiro 'C'
+    movq C, %rdi
+    call desalocaMem
+    call imprimeMapa              # após desalocar C
+    # Saída esperada:
+    # Imagem da heap: ################--------################---################----------
+
+    # Aloca 8 e armazena em 'A'
+    movq $8, TAMANHO
+    pushq TAMANHO
+    call alocaMem
+    addq $8, %rsp
+
+    movq %rax, A
+    call imprimeMapa              # após alocar A
+    # Saída esperada:
+    # Imagem da heap: ################++++++++################---################----------
+
+    # Aloca 10 e armazena em 'C'
+    movq $10, TAMANHO
+    pushq TAMANHO
+    call alocaMem
+    addq $8, %rsp
+
+    movq %rax, C
+    call imprimeMapa              # após alocar C
+    # Saída esperada:
+    # Imagem da heap: ################++++++++################---################++++++++++
+
+    # Aloca 3 e armazena em 'B'
+    movq $3, TAMANHO
+    pushq TAMANHO
+    call alocaMem
+    addq $8, %rsp
+
+    movq %rax, B
+    call imprimeMapa              # após alocar B
+    # Saída esperada:
+    # Imagem da heap: ################++++++++################+++################++++++++++
 
     # exit(0)
-    mov     $60, %rax
-    mov     $0, %rdi
+    movq $0, %rdi
+    movq $60, %rax
+    syscall
+
+erroMemoria:
+    # exit(1) ERRO em expandir %brk
+    movq $1, %rdi
+    movq $60, %rax
     syscall
